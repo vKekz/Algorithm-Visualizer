c5ee78940e75c429e9d7f3926d041b91b3eb2972
Kekz <kekzcrafter@protonmail.com>
2024-11-28 18:49:11 +0100

    Refactoring of theme-toggle, Fixed accent color of new sliders, Theme is saved after reload

--- algorithm-visualizer/src/app/app.component.ts
+++ algorithm-visualizer/src/app/app.component.ts
@@ -3,20 +3,19 @@ import { AlgorithmSelectorComponent } from "./components/algorithm-selector/algo
 import { VisualizerComponent } from "./components/visualizer/visualizer.component";
 import { AlgorithmOptionsComponent } from "./components/algorithm-options/algorithm-options.component";
 import { AlgorithmService } from "../services/algorithm.service";
+import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [AlgorithmSelectorComponent, VisualizerComponent, AlgorithmOptionsComponent],
+  imports: [AlgorithmSelectorComponent, VisualizerComponent, AlgorithmOptionsComponent, ThemeToggleComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   public readonly title: string = "Algorithm visualizer";
 
-  constructor(private readonly algorithmService: AlgorithmService) {
-    this.handleDOMContentLoaded();
-  }
+  constructor(private readonly algorithmService: AlgorithmService) {}
 
   public getTitle() {
     const algorithm = this.algorithmService.currentAlgorithm;
@@ -25,22 +24,5 @@ export class AppComponent {
     }
 
     return `${this.title} - ${algorithm.type}`;
-  }
-
-  private handleDOMContentLoaded() {
-    document.addEventListener('DOMContentLoaded', () => {
-      const checkbox = document.getElementById('togglecheckbox') as HTMLInputElement;
-      checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
-          document.documentElement.style.setProperty('--body-color', 'linear-gradient(to top right, #191d32, #333b52)');
-          document.documentElement.style.setProperty('--text-color', '#F7DD72');
-          document.documentElement.style.setProperty('--button-text-color', 'black');
-        }else{
-          document.documentElement.style.setProperty('--body-color', 'linear-gradient(to top right, #F7DD72, #CD956D)');
-          document.documentElement.style.setProperty('--text-color', 'black');
-          document.documentElement.style.setProperty('--button-text-color', '#F7DD72');
-        }   
-      });
-    });
   }
 }
--- algorithm-visualizer/src/services/options.service.ts
+++ algorithm-visualizer/src/services/options.service.ts
@@ -1,6 +1,7 @@
 ﻿import { DELAY_DEFAULT, ELEMENTS_DEFAULT } from "../constants/visualizer.constants";
 import { Injectable } from "@angular/core";
 import { fromEvent } from "rxjs";
+import { Theme } from "../enums/theme.enum";
 
 @Injectable({
   providedIn: "root",
@@ -10,9 +11,11 @@ export class OptionsService {
 
   private readonly delayKey: string = "delay";
   private readonly amountOfElementsKey: string = "amountOfElements";
+  private readonly themeKey: string = "theme";
 
   public delay: number = 0;
   public amountOfElements: number = 0;
+  public theme: Theme = 0;
 
   constructor() {
     fromEvent(window, this.BEFORE_UNLOAD_EVENT_NAME).subscribe(() => {
@@ -25,11 +28,13 @@ export class OptionsService {
   private handleSaving() {
     localStorage.setItem(this.delayKey, JSON.stringify(this.delay));
     localStorage.setItem(this.amountOfElementsKey, JSON.stringify(this.amountOfElements));
+    localStorage.setItem(this.themeKey, JSON.stringify(this.theme));
   }
 
   private loadOptions() {
     this.delay = this.getLocalDelay();
     this.amountOfElements = this.getLocalAmountOfElements();
+    this.theme = this.getLocalTheme();
   }
 
   private getLocalDelay() {
@@ -48,5 +53,16 @@ export class OptionsService {
     }
 
     return Number.parseInt(localAmountOfElements);
+  }
+
+  private getLocalTheme() {
+    const localTheme = localStorage.getItem(this.themeKey);
+    if (localTheme == null) {
+      return Theme.LIGHT;
+    }
+
+    return Object.keys(Theme)
+      .filter((x) => !isNaN(Number(x)))
+      .indexOf(localTheme);
   }
 }
